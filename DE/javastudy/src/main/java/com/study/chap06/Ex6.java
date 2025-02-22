27557d893c1fd277a54bbd84c773373a0d7ee9c3
RKDLDE <kangcanddy@gmail.com>
2025-02-22 10:38:24 +0900

    [add] 6-6 문제 풀이

---
+++ DE/javastudy/src/main/java/com/study/chap06/Ex6.java
@@ -0,0 +1,14 @@
+package com.study.chap06;
+
+/* 6-6. 두 점의 거리를 계산하는 getDistance()를 완성하시오. */
+public class Ex6 {
+    static double getDistance(int x, int y, int x1, int y1){
+        return Math.sqrt((x1-x)*(x1-x) + (y1-y)*(y1-y));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getDistance(1, 1, 2, 2));
+    }
+}
+
+// 1.4142135623730951
